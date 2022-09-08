dfd9eb03873cbbfe8dee813aca1bfe21ac512e3b
httpmurilo <muriloeduardo1997@outlook.com>
2022-09-08 20:13:20 -0300

    Inicio desenvolvimento do api gateway

---
+++ io.murilo.ApiGateway/src/main/java/io/murilo/Gateway/config/AppConfig.java
@@ -0,0 +1,31 @@
+package io.murilo.Gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@RefreshScope
+public class AppConfig {
+
+    @Value("${secret.jwt}")
+    private String secretJwt;
+
+    @Bean
+    public JwtAccessTokenConverter acessTokenConverter() {
+        var tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(secretJwt);
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(acessTokenConverter());
+    }
+
+
+
+}
--- io.murilo.Core/src/main/java/io/murilo/core/CoreApplication.java
+++ io.murilo.Core/src/main/java/io/murilo/core/CoreApplication.java
@@ -2,8 +2,12 @@ package io.murilo.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class CoreApplication {
 
 	public static void main(String[] args) {
